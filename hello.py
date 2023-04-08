16311088e4741324d3026345509e28ed48fdc007
johnjohnG <courir.carotte@gmail.com>
2023-04-08 12:11:28 +1000

    chap5.4

--- hello.py
+++ hello.py
@@ -4,11 +4,18 @@ from datetime import datetime
 from flask_wtf import FlaskForm
 from wtforms import StringField, SubmitField
 from wtforms.validators import DataRequired
+import os
+from flask_sqlalchemy import SQLAlchemy
+
+basedir = os.path.abspath(os.path.dirname(__file__))
 
 app = Flask(__name__)
 moment = Moment(app)
 app.config['SECRET_KEY'] = 'secret'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + os.path.join(basedir, 'data.sqlite')
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
+db = SQLAlchemy(app)
 
 @app.route('/', methods=['GET','POST'])
 def index():
@@ -39,3 +46,25 @@ def internal_server_error(e):
 class NameForm(FlaskForm):
     name = StringField('What is your name?', validators=[DataRequired()])
     submit = SubmitField('Submit')
+
+
+class Role(db.Model):
+    __tablename__ ='roles'
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(64), unique = True)
+
+    def __repr__(self):
+        return '<Role %r>' % self.name
+    
+    users = db.relationship('User', backref='role')
+
+
+class User(db.Model):
+    __tablename__ ='users'
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(64), unique = True, index=True)
+
+    def __repr__(self):
+        return '<Role %r>' % self.name
+    
+    role_id = db.Column(db.Integer, db.ForeignKey('roles.id'))
